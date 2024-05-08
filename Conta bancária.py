d29c2be0e65299842d9679c0ab86d5f051802b1b
Ronaldo <ronaldoroig@outlook.com>
2024-05-08 16:11:35 -0300

    refeito sistema com programacao modular
    clear

--- Conta bancária.py
+++ Conta bancária.py
@@ -1,59 +1,91 @@
+def menu():
+    menu="""
+    MENU
+    [d] - Depósitos
 
-menu="""
-MENU
-[d] - Depósitos
+    [s] - Saques
 
-[s] - Saques
+    [e] - Extrato
 
-[e] - Extrato
+    [q] - Sair
+    """
+    
+    return menu
 
-[q] - Sair
-"""
-deposito=[]
-saque=[]
-saldo=0.0
-limite_saque=0
+def depositar(valor_deposito):
+    if valor_deposito>0:
+        print("Depósito realizado com sucesso")
+        return valor_deposito
+    else:
+        print("Valor inválido, tente novamente")
 
-while True:
-
-    opcao=input(menu)
-
-    if opcao == "d":
-        valor_deposito=float(input("Informe valor a ser depositado: "))
-        deposito.append(valor_deposito)
-        saldo+=valor_deposito
-
-    elif opcao == "s":
-        if limite_saque>=3:
-            print("Limite de saque diário excedido")
-            continue
-        valor_saque=float(input("Informe valor a ser sacado: "))
-        if valor_saque>500:
-            print("Valor máximo de saque excedido, tente novamente")
-            continue
-        if valor_saque>saldo:
-            print("Não há saldo suficiente para realizar o saque")
-        saque.append(valor_saque)
-        saldo+=valor_saque*-1
-        limite_saque+=1
-
-    elif opcao == "e":
-        if deposito != [] or saque != []:
-            print("Histórico de depósitos")
-            for depositos in deposito:
-                print(f"R$ {depositos:.2f}")
-            print("Histórico de saques")
-            for saques in saque:
-                print(f"R$ {saques:.2f}")
-            print(f"Saldo da conta: R$ {saldo:.2f}")     
-        else:
-            print("Não foi realizado nenhuma operação")
-            continue
-            
-    elif opcao == "q":
-        break
+def sacar(saldo, valor_saque):
+    if valor_saque>500:
+        print("Valor máximo de saque excedido, tente novamente")
+    
+    elif valor_saque>saldo:
+        print("Não há saldo suficiente para realizar o saque")
 
     else:
-        print("Operação inválida, selecione uma das opções válidas")
+        print("Saque realizado com sucesso")
+        return valor_saque
 
+def extrato(deposito, saque, saldo):
+    mensagem=("Histórico de depósitos")
+    for depositos in deposito:
+        mensagem+=(f"\nR$ {depositos:.2f}")
+    mensagem+=("\nHistórico de saques")
+    for saques in saque:
+        mensagem+=(f"\nR$ {saques:.2f}")
+    mensagem+=(f"\nSaldo da conta: R$ {saldo:.2f}")
+
+    return mensagem
+    
+def main():
+
+    deposito=[]
+    saque=[]
+    saldo=0.0
+    limite_saque=0
+
+    while True:
+
+        print(menu())
+
+        opcao=input()
+
+        if opcao == "d":
+            valor_deposito=float(input("Informe valor a ser depositado: "))
+            resultado=depositar(valor_deposito)
+            deposito.append(resultado)
+            saldo+=resultado
+            
+        elif opcao == "s":
+            if limite_saque>=3:
+                print("Limite de saque diário excedido")
+                continue
+            else:
+                valor_saque=float(input("Informe valor a ser sacado: "))
+                resultado=sacar(saldo, valor_saque)
+                if resultado:
+                    saque.append(resultado)
+                    saldo-=resultado
+                else:
+                    continue
+
+        elif opcao == "e":
+            if deposito != [] or saque != []:
+                resultado=(extrato(deposito, saque, saldo))
+                print(resultado)
+            else:
+                print("Não foi realizado nenhuma operação")
+                continue
+                
+        elif opcao == "q":
+            break
+
+        else:
+            print("Operação inválida, selecione uma das opções válidas")
+
+main()
 
